91a20f763354d65bcc87dd952d291c3713cdd1e1
BraulioGaitan <brauliogaitan918@gmail.com>
2025-09-20 18:47:17 -0600

    producto final

--- calcularFiguras.py
+++ calcularFiguras.py
@@ -1,3 +1,23 @@
 import pandas as pd
+from figuras import rectangulo, triangulo, circulo
+
 df = pd.read_csv("figuras.csv")
-print("El archivo ha sido leido")
+print("El archivo ha sido leído\n")
+
+for index, row in df.iterrows():
+    figura = row["FIGURA"]
+    m1 = row["MEDIDA1"]
+    m2 = row["MEDIDA2"]
+
+    if figura == "r":
+        a, p = rectangulo(m1, m2)
+    elif figura == "t":
+        a, p = triangulo(m1, m2)
+    elif figura == "c":
+        a, p = circulo(m1)
+    else:
+        print(f"Figura desconocida en fila {index}")
+        continue
+
+    print(f"Fila {index}: FIGURA={figura} → Área={a}, Perímetro={p}")
+
--- figuras.py
+++ figuras.py
@@ -1,4 +1,16 @@
-def rectangulo (base,altura):
-	a = base * altura
-	p = 2 *(base+altura)
-	return a, p
+def rectangulo(base, altura):
+    a = base * altura
+    p = 2 * (base + altura)
+    return a, p
+
+def triangulo(base, altura):
+    a = (base * altura) / 2
+    l = (base**2 + altura**2) ** 0.5
+    p = l + base + altura
+    return a, p
+
+def circulo(radio):
+    a = 3.1416 * (radio**2)
+    p = 2 * 3.1416 * radio
+    return a, p
+
